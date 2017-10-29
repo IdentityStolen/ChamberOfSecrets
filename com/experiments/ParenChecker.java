1cc4f7a71238604dc360beeb720d458c777241a5
Trojan Azad <999999999cpk@gmail.com>
2017-10-29 22:03:38 +0530

    Code cleanup

--- com/experiments/ParenChecker.java
+++ com/experiments/ParenChecker.java
@@ -4,7 +4,7 @@ Checks if parenthesis are appropriate
  */
 package com.experiments;
 
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author SK030960
@@ -12,45 +12,89 @@ import java.util.Stack;
  */
 public class ParenChecker {
 
-	/**
-	 * @param args
-	 */
-	static boolean DoCheckParenthesis(String s) {
+	private static final Character OPENING_CURLY_BRACE = '{';
+	private static final Character CLOSING_CURLY_BRACE = '}';
+
+	private static final Character OPENING_SQUARE_BRACE = '[';
+	private static final Character CLOSING_SQUARE_BRACE = ']';
+
+	private static final Character OPENING_BRACE = '(';
+	private static final Character CLOSING_BRACE = ')';
+
+	private static final Collection<Character> ALL_PARENTHESIS_CHARS = new HashSet<>();
+	private static final Collection<Character> ALL_OPENING_PARENTHESIS_CHARS = new HashSet<>();
+	private static final Collection<Character> ALL_CLOSING_PARENTHESIS_CHARS = new HashSet<>();
+
+	private static final Map<Character,Parenthesis> closingBraceToParenthesisMap = new HashMap<>();
+
+	private enum Parenthesis {
+
+		CURLY(OPENING_CURLY_BRACE,CLOSING_CURLY_BRACE),
+		SQUARE(OPENING_SQUARE_BRACE,CLOSING_SQUARE_BRACE),
+		CIRCLE(OPENING_BRACE,CLOSING_BRACE);
+
+		private final Character opening;
+		private final Character closing;
+
+		Parenthesis(Character opening, Character closing) {
+			this.opening = opening;
+			this.closing = closing;
+
+			ALL_OPENING_PARENTHESIS_CHARS.add(opening);
+			ALL_PARENTHESIS_CHARS.add(opening);
+
+			ALL_CLOSING_PARENTHESIS_CHARS.add(closing);
+			ALL_PARENTHESIS_CHARS.add(closing);
+
+			closingBraceToParenthesisMap.put(closing,this);
+		}
+
+		public Character getOpening() {
+			return opening;
+		}
+
+		public Character getClosing() {
+			return closing;
+		}
+	}
+
+	static boolean isOpeningBraceAvailableForCorrespondingClosingBrace(Parenthesis parenthesis,Stack<Character> stack,Character c){
+		if (parenthesis.getClosing().equals(c)) {
+			if (!stack.isEmpty()) {
+				Character c1 = stack.pop();
+				if (c1 != parenthesis.getOpening()) return false;
+			}
+			else return false;
+		}
+		return false;
+	}
+
+
+	static boolean areParenthesisBalanced(String s) {
 		char c1 = 'a';
-		Stack stack = new Stack();
+		Stack<Character> stack = new Stack<>();	//Never compromise on type safety.
 		
 		char[] stringArray = s.toCharArray();
-		for (char c : stringArray) {
-			if ((c == '{') || (c == '[') || (c == '(')) stack.push(c);
-			else if (c == '}') {
-				if (!stack.isEmpty()) {
-					c1 = (char) stack.pop();
-					if (c1 != '{') return false;
+		for (Character c : stringArray) {
+			if (isValidClosingParenthesisChar(c)) stack.push(c);
+			else {
+
+				Parenthesis parenthesis = closingBraceToParenthesisMap.get(c);
+				if(parenthesis!=null){
+					return isOpeningBraceAvailableForCorrespondingClosingBrace(parenthesis, stack, c);
 				}
-				else return false;
-			}
-			else if (c == ']') {
-				if (!stack.isEmpty()) {
-					c1 = (char) stack.pop();
-					if (c1 != '[') return false;
-				}
-				else return false;
-			}
-			else if (c == ')') {
-				if (!stack.isEmpty()) {
-					c1 = (char) stack.pop();
-					if (c1 != '(') return false;
-				}
-				else return false;
 			}
 		}
-		if (!stack.isEmpty()) return false;
-		return true;
+		return stack.isEmpty();
 	}
-	
+
+	public static boolean isValidClosingParenthesisChar(Character c){
+		return ALL_CLOSING_PARENTHESIS_CHARS.contains(c);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(DoCheckParenthesis("aa bb {[[]]} cc + dd [[[]]]"));
+		System.out.println(areParenthesisBalanced("aa bb {[[]]} cc + dd [[[]]]"));
 	}
 
 }
